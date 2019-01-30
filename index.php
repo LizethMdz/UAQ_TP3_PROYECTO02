ee08493a47c0bca89e3ec945a3f146d1dab73ee4
Lizeth1426 <lizethailet@hotmail.com>
2019-01-29 20:35:33 -0600

    update

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="CSS/estilos.css">
+  <link rel="shortcut icon" type="image/png" href="IMG/habilidad.png">
+  <title>PROYECTO 1</title>
+</head>
+
+<body>
+  <header>
+    <div class="collapse bg-dark" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">Acerca de</h4>
+            <p class="text-muted">
+              El sitio tiene como proposito presentar una encuesta
+              a varios usuarios, con el proposito de identirficar el
+              indice de proactividad.
+            </p>
+          </div>
+          <div class="col-sm-4 offset-md-1 py-4">
+            <h4 class="text-white">Contacto</h4>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white">Follow us on Twitter</a></li>
+              <li><a href="#" class="text-white">Like on Facebook</a></li>
+              <li><a href="#" class="text-white">Email us</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark shadow-sm bg-nav">
+      <div class="container d-flex justify-content-between">
+        <!-- <a href="index.php"><img src="IMG/lamtsoft.png" height="150px" alt="LAMTSOFT"></a> -->
+        <a href="index.php" class="navbar-brand d-flex align-items-center">
+          <strong>SISTEMAS INFORMATICOS</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </div>
+  </header>
+
+
+  <br><br>
+  <main role="main">
+
+
+    <section class=" text-center bg-white">
+      <div class="container">
+        <h1 class="jumbotron-heading">TÓPICO III - </h1>
+        <img src="IMG/lamtsoft.png" alt="LAMTSOFT">
+        <p class="lead text-muted">7 HABITOS DE LA GENTE ALTAMAMENTE EFECTIVA
+        </p>
+        <h6 class="text-animation">Los hábitos son factores poderosos en nuestras vidas. Dado que son pautas consistentes,
+          a menudo inconscientes, de modo constante y
+          cotidiano expresan nuestro carácter y generan nuestra efectividad o inefectividad.</h6>
+      </div>
+    </section>
+
+    <hr>
+    <div class="container col-sm-10">
+      <h4 class="text-center">Proyecto I </h4>
+      <h4 class="text-center">21-01-2019</h4>
+      <br><br>
+      <div class="col-sm-12 img-inicio">
+        <img src="IMG/proactivo.png" alt="proactividad">
+      </div>
+    </div>
+
+    <div class="container col-sm-10 text-center btn-next">
+      <a class="btn btn-success btn-lg text-white" href="menu.php">Siguiente</a>
+    </div>
+
+  </main>
+  <br><br>
+  <hr>
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Regresar arriba</a>
+      </p>
+      <p>Copyright Reserved</p>
+      <p>New on this page <a href="#">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+    </div>
+  </footer>
+
+
+  <!-- SCRIPTS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
